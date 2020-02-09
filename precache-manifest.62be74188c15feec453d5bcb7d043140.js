914d540c420c28dd5d6a979da3084bd61b938a87
deluxscript <delsam94@yahoo.com>
2020-02-09 07:42:43 +0100

    Updates

---
+++ precache-manifest.62be74188c15feec453d5bcb7d043140.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9892a54fd9046f172d0cf3ac1892caa",
+    "url": "/e-iv/index.html"
+  },
+  {
+    "revision": "833c650daadfe14b5db5",
+    "url": "/e-iv/static/css/main.f550fda1.chunk.css"
+  },
+  {
+    "revision": "87b8cc249b18b963cdea",
+    "url": "/e-iv/static/js/2.3c5fb3c0.chunk.js"
+  },
+  {
+    "revision": "504111a2853a0142fe94b3aa362f3e70",
+    "url": "/e-iv/static/js/2.3c5fb3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833c650daadfe14b5db5",
+    "url": "/e-iv/static/js/main.2816c0ca.chunk.js"
+  },
+  {
+    "revision": "16d1c4f53e1a3a68ebcd",
+    "url": "/e-iv/static/js/runtime-main.5b900dc3.js"
+  },
+  {
+    "revision": "de9dcffc61fc4ffc95bbd8d8c90a0e6c",
+    "url": "/e-iv/static/media/branch.de9dcffc.svg"
+  },
+  {
+    "revision": "1fe9a143c3f3c98d4d00aa61fddd6d48",
+    "url": "/e-iv/static/media/pattern.1fe9a143.jpg"
+  },
+  {
+    "revision": "71b02ab5bd299a1b80dacd32f3515435",
+    "url": "/e-iv/static/media/wedding-planning.71b02ab5.png"
+  }
+]);
